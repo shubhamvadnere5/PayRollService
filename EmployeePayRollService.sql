77daec5d47a55c169c8b6958a4eccbd9250e2042
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-26 13:35:04 +0530

    [ShubhamPV]  Create table emppayroll

--- EmployeePayRollService.sql
+++ EmployeePayRollService.sql
@@ -1,2 +1,14 @@
 --UC1 Create Database--
 Create Database EmployeePayRollService;
+
+
+--UC2 Create Table--
+use EmployeePayRollService;
+create table  emppayroll(
+      Id int IDENTITY(1,1) PRIMARY KEY, 
+      Name varchar(200),
+      Salary float,
+      StartDate date
+);
+
+
