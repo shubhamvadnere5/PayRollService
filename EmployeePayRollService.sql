b4a24c8792dc2bfcd622422b119b0b49f3896d77
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-26 14:36:31 +0530

    [ShubhamPV]  extend emp payroll table

--- EmployeePayRollService.sql
+++ EmployeePayRollService.sql
@@ -39,3 +39,13 @@ alter table emppayroll add Emp_Phone_no bigint ;
 alter table emppayRoll add Department varchar(50) not null default 'HR' ;
 update emppayroll set Department='IT' where ID=1;
 alter table emppayRoll add Address varchar(100) default 'Mumbai';
+ 
+--Uc9---
+alter table emppayroll add TaxablePay float, Deduction float,IncomeTax float,NetPay float;
+Update emppayroll set Deduction = '4000' where Department = 'HR';
+Update emppayroll set Deduction = '3000' where Department = 'Sales';
+Update emppayroll set Deduction = '2000' where Department = 'Customer Service';
+Update emppayroll set NetPay = (NetPay-Deduction);
+Update emppayroll set TaxablePay = '1000';
+Update emppayroll set IncomeTax = '200';
+select * from emppayroll;
